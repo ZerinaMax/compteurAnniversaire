0e3329cab8641cb6d10328e0bd636c858759ef30
[ZerinaMax] <zerina.maxhuni@gmail.com>
2022-06-30 10:06:01 +0200

    first try

---
+++ displayDate.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/assets/css/style.css">
+    <title>Affiche Jours restants avant Anniversaire</title>
+</head>
+<body>
+    <?php
+    if (file_exists("displayDate.php")) {
+        echo "
+        <img src='/assets/img/image_1417.jpg' alt=''>
+        <span> Cher visiteur, votre anniversaire est dans </span>
+        
+        
+        
+        "
+    }
+
+    ?>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/assets/css/style.css">
+    <title>Compteur Anniversaire</title>
+</head>
+<body>
+    <?php
+        echo " 
+            <h1> Calcul d'anniversaire </h1>
+            <form action='displayDate.php' method='POST'>
+                <h3> Pourriez-vous nous indiquer votre prénom </h3> <input type='text' name='prenom'> <br> <br>
+            <fieldset>
+                <legend> Veuillez compléter les champs ci-après </legend> <br>
+                <div> 
+                    <span> Jour de naissance </span>   <input type='text' name='jourAnniv'> <span> (en chiffres)</span> <br> <br>
+                </div>
+                <div> 
+                    <span> Mois de naissance </span>   <input type='text' name='jourAnniv'> <span> (en chiffres)</span> 
+                </div>
+                <input type='hidden' name='form_submitted' value='1' />
+                <input type='submit' name='calcul' value='Calculer'>
+            </form>
+            </fieldset>
+        "
+    ?>
+</body>
+</html>
